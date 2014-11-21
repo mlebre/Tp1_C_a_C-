5ac6fc991b41ea5dad776c3de0a83e8cc474be4d
Lebre Marianne <mlebre@bs406-s33-19.insa-lyon.fr>
2014-11-21 09:24:05 +0100

    File to test modifications about passage into C++# Veuillez saisir le message de validation pour vos modifications. t modies lignes

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ppm_file.h"
+#include "image.h"
+
+int main()
+{
+
+ printf("Hello world!\n");
+   
+  return 0;
+}
